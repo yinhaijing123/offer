1a09c0b777d3729ac4bb5f73536741bbb652f9b4
yinhaijing123 <2192705954@qq.com>
2020-05-13 16:00:24 +0800

    [Modify][What] AssignmentOperator.cpp

--- AssignmentOperator/AssignmentOperator.cpp
+++ AssignmentOperator/AssignmentOperator.cpp
@@ -18,7 +18,7 @@ public:
         m_pData =new char[strlen(str.m_pData)+1];
         strcpy(this->m_pData,str.m_pData);
     }
-	
+	//法1：实现需考虑四方面：1.返回值（为连续赋值，返回值类型为类名的引用）；2.参数（提高效率，参数传引用类型，避免不必要的拷贝开销，同时参数声明为常量引用，即const，避免复制时误改参数值）；3.对象自己给自己赋值情况（特殊情况，特殊处理直接返回*this）；4.释放之前空间后，再进行赋值（避免内存泄漏）。
     CMyString& operator=(const CMyString &str)
     {
         cout<<"operator="<<endl;
@@ -33,7 +33,22 @@ public:
         strcpy(m_pData,str.m_pData);
         return *this;
     }
-	
+	//法2：对法1的优化。利用局部变量出作用域会自动调用析构函数思想，调用拷贝构造函数创建局部变量，将被赋值数所指向的空间与局部变量进行交换，让系统进行自动释放。
+	/*CMyString& MyString::operator=(const CMyString& str)
+	{
+		if (this != &str)
+		{
+			MyString tmp(str);
+			swap(tmp.m_pData, m_pData);
+		}
+		return *this;
+	}*/
+    //法3：对法2的优化。直接利用传参时调用拷贝构造函数构造临时的str，交换两者。
+	/*CMyString& MyString::operator=(CMyString str)
+	{
+		swap(str.m_pData, m_pData);
+		return *this;
+	}*/
     void Show()
     {
         cout<<m_pData<<endl;
